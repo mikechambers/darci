1a821c495e626ff396d5f02545eea477c452fd7d
Mike Chambers <mikechambers@gmail.com>
2022-07-11 09:41:02 -0700

    Add time played view and human duration function

--- client-web/src/pages/player/components/TimePlayedView.js
+++ client-web/src/pages/player/components/TimePlayedView.js
@@ -1,6 +1,5 @@
-import { Duration } from "luxon";
-
 import React from "react";
+import { humandDuration } from "../../../utils/date";
 
 const TimePlayedView = (props) => {
   let seconds = props.seconds;
@@ -9,50 +8,7 @@ const TimePlayedView = (props) => {
     return "";
   }
 
-  //var dur = Duration.fromMillis({ count: seconds * 1000 });
-  var dur = Duration.fromMillis(seconds * 1000)
-    .shiftTo("years", "months", "days", "hours", "minutes", "seconds")
-    .toObject();
-
-  let humanDurationParts = [];
-
-  let f = function (data, label) {
-    if (!data) {
-      return null;
-    }
-
-    let out = `${data} ${label}`;
-
-    if (data > 1) {
-      out += "s";
-    }
-
-    out += ",";
-    return out;
-  };
-
-  humanDurationParts.push(f(dur.years, "year"));
-  humanDurationParts.push(f(dur.months, "month"));
-  humanDurationParts.push(f(dur.days, "day"));
-  humanDurationParts.push(f(dur.hours, "hour"));
-  humanDurationParts.push(f(dur.minutes, "minute"));
-  humanDurationParts.push(f(dur.seconds, "second"));
-
-  humanDurationParts = humanDurationParts.filter((entry) => entry !== null);
-
-  if (humanDurationParts.length > 1) {
-    humanDurationParts[humanDurationParts.length - 2] = humanDurationParts
-      .at(-2)
-      .slice(0, -1);
-
-    humanDurationParts.splice(humanDurationParts.length - 1, 0, "and");
-
-    humanDurationParts[humanDurationParts.length - 1] = humanDurationParts
-      .at(-1)
-      .slice(0, -1);
-  }
-
-  let human = humanDurationParts.join(" ");
+  let human = humandDuration(seconds * 1000);
 
   return <div>Total time played is {human}</div>;
 };
--- client-web/src/utils/date.js
+++ client-web/src/utils/date.js
@@ -1,3 +1,5 @@
+import { Duration } from "luxon";
+
 export const dateIsToday = function (d) {
   let n = new Date();
   return (
@@ -12,4 +14,50 @@ export const dateIsWithinLastWeek = function (d) {
   let diffMs = Math.abs(d.getTime() - n.getTime());
   const weekMs = 7 * 24 * 60 * 60 * 1000;
   return diffMs > weekMs;
+};
+
+export const humandDuration = function (ms) {
+  var dur = Duration.fromMillis(ms)
+    .shiftTo("years", "months", "days", "hours", "minutes", "seconds")
+    .toObject();
+
+  let f = function (data, label) {
+    if (!data) {
+      return null;
+    }
+
+    let out = `${data} ${label}`;
+
+    if (data > 1) {
+      out += "s";
+    }
+
+    out += ",";
+    return out;
+  };
+
+  let humanDurationParts = [];
+  humanDurationParts.push(f(dur.years, "year"));
+  humanDurationParts.push(f(dur.months, "month"));
+  humanDurationParts.push(f(dur.days, "day"));
+  humanDurationParts.push(f(dur.hours, "hour"));
+  humanDurationParts.push(f(dur.minutes, "minute"));
+  humanDurationParts.push(f(dur.seconds, "second"));
+
+  humanDurationParts = humanDurationParts.filter((entry) => entry !== null);
+
+  if (humanDurationParts.length > 1) {
+    //remove the comma before the and
+    humanDurationParts[humanDurationParts.length - 2] = humanDurationParts
+      .at(-2)
+      .slice(0, -1);
+
+    humanDurationParts.splice(humanDurationParts.length - 1, 0, "and");
+
+    humanDurationParts[humanDurationParts.length - 1] = humanDurationParts
+      .at(-1)
+      .slice(0, -1);
+  }
+
+  return humanDurationParts.join(" ");
 };
