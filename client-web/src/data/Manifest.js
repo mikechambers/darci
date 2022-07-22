10f2266c0042bfa3645a881a6dafe86a6daa737a
Mike Chambers <mikechambers@gmail.com>
2022-07-21 18:09:19 -0700

    better defaults for missing weapon / manifest data

--- client-web/src/data/Manifest.js
+++ client-web/src/data/Manifest.js
@@ -2,196 +2,201 @@ const { ItemSubType } = require("shared");
 
 const API_RESOURCE_BASE_URL = "https://www.bungie.net";
 class Manifest {
+  #manifestData;
 
-    #manifestData;
+  #trialsPassageIds = [];
 
-    #trialsPassageIds = [];
+  constructor(manifestData) {
+    this.#manifestData = manifestData;
 
+    for (const [key, value] of Object.entries(
+      this.#manifestData.data.trialsPassageItemDefinitions
+    )) {
+      if (value.id) {
+        this.#trialsPassageIds.push(value.id);
+      }
+    }
+  }
 
-    constructor(manifestData) {
-        this.#manifestData = manifestData;
+  getWeaponDefinition(id) {
+    let out = {
+      name: "Unknown",
+      itemType: undefined,
+      itemSubType: ItemSubType.UNKNOWN,
+      id: id,
+      icon: "https://www.bungie.net/common/destiny2_content/icons/528d5264b28c155e1bba26afb427aba0.png",
+      screenshot: undefined,
+    };
 
-        for (const [key, value] of Object.entries(this.#manifestData.data.trialsPassageItemDefinitions)) {
-            if (value.id) {
-                this.#trialsPassageIds.push(value.id);
-            }
-        }
+    if (!this.#manifestData.data.weaponItemDefinition) {
+      return out;
     }
 
-    getWeaponDefinition(id) {
-        let out = {
-            name: undefined,
-            itemType: undefined,
-            itemSubType: undefined,
-            id: id,
-            icon: undefined,
-            screenshot: undefined,
-        };
+    let d = this.#manifestData.data.weaponItemDefinition[id];
 
-
-        if (!this.#manifestData.data.weaponItemDefinition) {
-            return out;
-        }
-
-        let d = this.#manifestData.data.weaponItemDefinition[id];
-
-        if (!d) {
-            return out;
-        }
-
-        Object.assign(out, d);
-        out.itemSubType = ItemSubType.fromId(d.itemSubType);
-        out.icon = createResourceUrl(d.icon);
-        out.screenshot = createResourceUrl(d.screenshot);
-
-
-        return out;
+    if (!d) {
+      return out;
     }
 
-    getActivityDefinition(id) {
+    Object.assign(out, d);
 
-        let out = {
-            name: "Unknown",
-            image: undefined,
-            directActivityModeHash: undefined,
-            id: id,
-        };
+    out = {
+      ...out,
+      ...d,
+    };
 
-        if (!this.#manifestData.data.activityDefinition) {
-            return out;
-        }
+    out.itemSubType = ItemSubType.fromId(d.itemSubType);
+    out.icon = createResourceUrl(d.icon);
+    out.screenshot = createResourceUrl(d.screenshot);
 
-        let d = this.#manifestData.data.activityDefinition[id];
+    return out;
+  }
 
-        if (!d) {
-            return out;
-        }
+  getActivityDefinition(id) {
+    let out = {
+      name: "Unknown",
+      image: undefined,
+      directActivityModeHash: undefined,
+      id: id,
+    };
 
-        Object.assign(out, d);
-        out.image = createResourceUrl(d.image);
-
-        return out;
+    if (!this.#manifestData.data.activityDefinition) {
+      return out;
     }
 
-    getTrialsPassageDefinition(id) {
-        let out = {
-            name: "Unknown",
-            description: undefined,
-            icon: undefined,
-            id: id,
-        }
+    let d = this.#manifestData.data.activityDefinition[id];
 
-        if (!this.#manifestData.data.trialsPassageItemDefinitions) {
-            return out;
-        }
-
-        let d = this.#manifestData.data.trialsPassageItemDefinitions[id];
-
-        Object.assign(out, d);
-        out.icon = createResourceUrl(d.icon);
-
-        return (!d) ? out : d;
+    if (!d) {
+      return out;
     }
 
-    get trialsPassageIds() {
-        return this.#trialsPassageIds;
+    Object.assign(out, d);
+    out.image = createResourceUrl(d.image);
+
+    return out;
+  }
+
+  getTrialsPassageDefinition(id) {
+    let out = {
+      name: "Unknown",
+      description: undefined,
+      icon: undefined,
+      id: id,
+    };
+
+    if (!this.#manifestData.data.trialsPassageItemDefinitions) {
+      return out;
     }
 
-    getEmblemDefinition(id) {
+    let d = this.#manifestData.data.trialsPassageItemDefinitions[id];
 
-        let out = {
-            id: id,
-            name: "Unknown",
-            icon: undefined,
-            secondaryIcon: undefined,
-            secondaryOverlay: undefined,
-            secondarySpecial: undefined,
-        };
+    Object.assign(out, d);
+    out.icon = createResourceUrl(d.icon);
 
-        if (!this.#manifestData.data.emblemDefinitions) {
-            return out;
-        }
+    return !d ? out : d;
+  }
 
-        let e = this.#manifestData.data.emblemDefinitions[id]
+  get trialsPassageIds() {
+    return this.#trialsPassageIds;
+  }
 
-        if (!e) {
-            return out;
-        }
+  getEmblemDefinition(id) {
+    let out = {
+      id: id,
+      name: "Unknown",
+      icon: undefined,
+      secondaryIcon: undefined,
+      secondaryOverlay: undefined,
+      secondarySpecial: undefined,
+    };
 
-        Object.assign(out, e);
-        out.icon = createResourceUrl(e.icon);
-        out.secondaryIcon = createResourceUrl(e.secondaryIcon);
-        out.secondaryOverlay = createResourceUrl(e.secondaryOverlay);
-        out.secondarySpecial = createResourceUrl(e.secondarySpecial);
-
-        return out;
+    if (!this.#manifestData.data.emblemDefinitions) {
+      return out;
     }
 
-    getModeInfo(id) {
-        let out = {
-            name: "Unknown",
-            description: undefined,
-            icon: undefined,
-            image: undefined,
-            id: id,
-        }
+    let e = this.#manifestData.data.emblemDefinitions[id];
 
-        let a = this.getActivityDefinition(id);
-        if (!a) {
-            console.log(`Manifest.getModeInfo : Could not find activity definition [${id}]`);
-            return out;
-        }
-
-        if (!this.#manifestData.data.activityModeDefinitions) {
-            return out;
-        }
-
-        let m = this.#manifestData.data.activityModeDefinitions[a.activityModeHash];
-        if (!m) {
-            console.log(`Manifest.getModeInfo : Could not find activity mode definition [${a.activityModeHash}]`);
-            return out;
-        }
-
-        Object.assign(out, m);
-        out.icon = createResourceUrl(m.icon);
-        out.image = createResourceUrl(m.image);
-
-        return out;
+    if (!e) {
+      return out;
     }
 
-    getMedalDefinition(id) {
-        let out = {
-            name: "Unknown",
-            description: undefined,
-            icon: undefined,
-            isGold: false,
-            id: id,
-        };
+    Object.assign(out, e);
+    out.icon = createResourceUrl(e.icon);
+    out.secondaryIcon = createResourceUrl(e.secondaryIcon);
+    out.secondaryOverlay = createResourceUrl(e.secondaryOverlay);
+    out.secondarySpecial = createResourceUrl(e.secondarySpecial);
 
-        if (!this.#manifestData.data.medalDefinitions) {
-            return out;
-        }
+    return out;
+  }
 
-        let m = this.#manifestData.data.medalDefinitions[id];
+  getModeInfo(id) {
+    let out = {
+      name: "Unknown",
+      description: undefined,
+      icon: undefined,
+      image: undefined,
+      id: id,
+    };
 
-        if (!m) {
-            return out;
-        }
-
-        Object.assign(out, m);
-        out.icon = createResourceUrl(m.icon);
-
-        return out;
+    let a = this.getActivityDefinition(id);
+    if (!a) {
+      console.log(
+        `Manifest.getModeInfo : Could not find activity definition [${id}]`
+      );
+      return out;
     }
+
+    if (!this.#manifestData.data.activityModeDefinitions) {
+      return out;
+    }
+
+    let m = this.#manifestData.data.activityModeDefinitions[a.activityModeHash];
+    if (!m) {
+      console.log(
+        `Manifest.getModeInfo : Could not find activity mode definition [${a.activityModeHash}]`
+      );
+      return out;
+    }
+
+    Object.assign(out, m);
+    out.icon = createResourceUrl(m.icon);
+    out.image = createResourceUrl(m.image);
+
+    return out;
+  }
+
+  getMedalDefinition(id) {
+    let out = {
+      name: "Unknown",
+      description: undefined,
+      icon: undefined,
+      isGold: false,
+      id: id,
+    };
+
+    if (!this.#manifestData.data.medalDefinitions) {
+      return out;
+    }
+
+    let m = this.#manifestData.data.medalDefinitions[id];
+
+    if (!m) {
+      return out;
+    }
+
+    Object.assign(out, m);
+    out.icon = createResourceUrl(m.icon);
+
+    return out;
+  }
 }
 
 const createResourceUrl = (path) => {
-    if (!path) {
-        return undefined;
-    }
+  if (!path) {
+    return undefined;
+  }
 
-    return `${API_RESOURCE_BASE_URL}${path}`;
-}
+  return `${API_RESOURCE_BASE_URL}${path}`;
+};
 
 export default Manifest;
-
