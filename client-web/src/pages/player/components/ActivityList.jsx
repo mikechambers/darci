c2cb478819c5d9074c807dc1e90ab91210578ff4
Mike Chambers <mikechambers@gmail.com>
2022-07-22 18:25:20 -0700

    Start refactor of activity list to add detail drawer

--- client-web/src/pages/player/components/ActivityList.jsx
+++ client-web/src/pages/player/components/ActivityList.jsx
@@ -1,12 +1,6 @@
-import { CompletionReason, Standing } from "shared";
 import ExportDataButton from "../../../components/ExportDataButton";
 import GraphicListHeader from "../../../components/GraphicListHeader";
-import JoinedLateIcon from "../../../components/JoinedLateIcon";
-import JoinedLeftIcon from "../../../components/JoinedLeftIcon";
-import LeftEarlyIcon from "../../../components/LeftEarlyIcon";
-import MercyIcon from "../../../components/MercyIcon";
-
-import Stat from "./Stat";
+import ActivityListItem from "./ActivityListItem";
 
 const WIDTH = 735;
 
@@ -22,63 +16,6 @@ const wrapperStyle = {
   flexDirection: "column",
   /*gap: "var(--list-item-gap)"*/
   gap: "2px",
-};
-
-const gameContainerStyle = {
-  display: "flex",
-  flexDirection: "row",
-  width: `${WIDTH}px`,
-  height: "46px",
-  backgroundColor: "var(--list-item-background-color)",
-  border: "var(--list-item-border)",
-  borderLeftWidth: "0px",
-};
-
-const resultWinStyle = {
-  backgroundColor: "#3FD445",
-  width: "4px",
-};
-
-const resultLossStyle = {
-  backgroundColor: "#E92626",
-  width: "4px",
-};
-
-const gameTitleStyle = {
-  display: "flex",
-  flexDirection: "column",
-  width: "160px",
-  justifyContent: "center",
-  paddingLeft: "4px",
-};
-
-const statsStyle = {
-  //display: "flex",
-  //flexDirection: "row",
-  //justifyContent: "space-between",
-  width: "480px",
-
-  alignItems: "center",
-
-  display: "grid",
-  gridTemplateColumns: "repeat(6, 1fr)",
-};
-
-const statusStyle = {
-  width: "35px",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-};
-
-const medalsStyle = {
-  width: "60px",
-  display: "flex",
-  justifyContent: "flex-start",
-  alignItems: "center",
-  flexWrap: "wrap",
-  columnGap: "4px",
 };
 
 const footerStyle = {
@@ -103,136 +40,8 @@ const ActivityList = (props) => {
 
       <div style={wrapperStyle}>
         {activities.map((game, index) => {
-          let resultStyle =
-            game.stats.standing === Standing.VICTORY
-              ? resultWinStyle
-              : resultLossStyle;
-
-          let dimen = 10;
-          let mercyIcon =
-            game.stats.completionReason === CompletionReason.MERCY ? (
-              <MercyIcon height={dimen} width={dimen} />
-            ) : (
-              ""
-            );
-
-          let statusIcon = "";
-          if (!game.stats.completed) {
-            statusIcon = <LeftEarlyIcon height={dimen} width={dimen} />;
-          }
-
-          if (game.stats.joinedLate) {
-            statusIcon = <JoinedLateIcon height={dimen} width={dimen} />;
-          }
-
-          if (!game.stats.completed && game.stats.joinedLate) {
-            statusIcon = <JoinedLeftIcon height={dimen} width={dimen} />;
-          }
-
-          let gm = new Map();
-          for (let m of game.stats.extended.medals) {
-            if (m.info.isGold) {
-              gm.set(m.id, m.info);
-            }
-          }
-
-          let goldMedals = Array.from(gm.values());
-
-          if (goldMedals.length > 6) {
-            goldMedals = goldMedals.slice(0, 5);
-          }
-
-          let title = "Highest Value";
-          let killsHighlight = game.stats.kills === summary.highestKills;
-          let killsTitle = killsHighlight ? title : "";
-
-          let assistsHighlight = game.stats.assists === summary.highestAssists;
-          let assistsTitle = assistsHighlight ? title : "";
-
-          let defeatsHighlight =
-            game.stats.opponenentsDefeated === summary.highestOpponentsDefeated;
-          let defeatsTitle = defeatsHighlight ? title : "";
-
-          let deathsHighlight = game.stats.deaths === summary.highestDeaths;
-          let deathsTitle = deathsHighlight ? title : "";
-
-          let kdHighlight =
-            game.stats.killsDeathsRatio.toFixed(2) ===
-            summary.highestKillsDeathsRatio.toFixed(2);
-          let kdTitle = kdHighlight ? title : "";
-
-          let effHighlight =
-            game.stats.efficiency.toFixed(2) ===
-            summary.highestEfficiency.toFixed(2);
-          let effTitle = effHighlight ? title : "";
-
           return (
-            <div style={gameContainerStyle} key={index}>
-              <div style={resultStyle}></div>
-              <div style={gameTitleStyle}>
-                <div className="list_title">{game.activity.map.name}</div>
-                <div className="list_subtitle">{game.activity.mode.label}</div>
-              </div>
-
-              <div style={statsStyle}>
-                <Stat
-                  label="kills"
-                  value={game.stats.kills}
-                  highlight={killsHighlight}
-                  title={killsTitle}
-                />
-                <Stat
-                  label="assists"
-                  value={game.stats.assists}
-                  highlight={assistsHighlight}
-                  title={assistsTitle}
-                />
-                <Stat
-                  label="defeats"
-                  value={game.stats.opponentsDefeated}
-                  highlight={defeatsHighlight}
-                  title={defeatsTitle}
-                />
-                <Stat
-                  label="deaths"
-                  value={game.stats.deaths}
-                  highlight={deathsHighlight}
-                  title={deathsTitle}
-                />
-                <Stat
-                  label="kd"
-                  value={game.stats.killsDeathsRatio.toFixed(2)}
-                  highlight={kdHighlight}
-                  title={kdTitle}
-                />
-                <Stat
-                  label="eff"
-                  value={game.stats.efficiency.toFixed(2)}
-                  highlight={effHighlight}
-                  title={effTitle}
-                />
-              </div>
-
-              <div style={statusStyle}>
-                <div>{statusIcon}</div>
-                <div>{mercyIcon}</div>
-              </div>
-              <div style={medalsStyle}>
-                {goldMedals.map((medal, index) => {
-                  return (
-                    <img
-                      key={index}
-                      src={medal.icon}
-                      alt={medal.description}
-                      title={`${medal.name.toUpperCase()} - ${
-                        medal.description
-                      }`}
-                      height="14"
-                    />
-                  );
-                })}
-              </div>
-            </div>
+            <ActivityListItem activity={game} summary={summary} key={index} />
           );
         })}
       </div>
---
+++ client-web/src/pages/player/components/ActivityListItem.jsx
@@ -0,0 +1,245 @@
+import { useState } from "react";
+import { CompletionReason, Standing } from "shared";
+import JoinedLateIcon from "../../../components/JoinedLateIcon";
+import JoinedLeftIcon from "../../../components/JoinedLeftIcon";
+import LeftEarlyIcon from "../../../components/LeftEarlyIcon";
+import MercyIcon from "../../../components/MercyIcon";
+import Stat from "./Stat";
+
+const resultWinStyle = {
+  backgroundColor: "#3FD445",
+  width: "4px",
+};
+
+const resultLossStyle = {
+  backgroundColor: "#E92626",
+  width: "4px",
+};
+
+const statsStyle = {
+  //display: "flex",
+  //flexDirection: "row",
+  //justifyContent: "space-between",
+  width: "480px",
+
+  alignItems: "center",
+
+  display: "grid",
+  gridTemplateColumns: "repeat(6, 1fr)",
+};
+
+//todo: remove this here or in parent
+const WIDTH = 735;
+
+const gameContainerStyle = {
+  display: "flex",
+  flexDirection: "row",
+  width: `${WIDTH}px`,
+  height: "46px",
+  backgroundColor: "var(--list-item-background-color)",
+  border: "var(--list-item-border)",
+  borderLeftWidth: "0px",
+};
+
+const gameTitleStyle = {
+  display: "flex",
+  flexDirection: "column",
+  width: "160px",
+  justifyContent: "center",
+  paddingLeft: "4px",
+};
+
+const gameContainerWrapper = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+};
+
+const statusStyle = {
+  width: "35px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const medalsStyle = {
+  width: "60px",
+  display: "flex",
+  justifyContent: "flex-start",
+  alignItems: "center",
+  flexWrap: "wrap",
+  columnGap: "4px",
+};
+
+const ActivityListItem = (props) => {
+  let activity = props.activity;
+  let summary = props.summary;
+
+  let resultStyle =
+    activity.stats.standing === Standing.VICTORY
+      ? resultWinStyle
+      : resultLossStyle;
+
+  let dimen = 10;
+  let mercyIcon =
+    activity.stats.completionReason === CompletionReason.MERCY ? (
+      <MercyIcon height={dimen} width={dimen} />
+    ) : (
+      ""
+    );
+
+  let statusIcon = "";
+  if (!activity.stats.completed) {
+    statusIcon = <LeftEarlyIcon height={dimen} width={dimen} />;
+  }
+
+  if (activity.stats.joinedLate) {
+    statusIcon = <JoinedLateIcon height={dimen} width={dimen} />;
+  }
+
+  if (!activity.stats.completed && activity.stats.joinedLate) {
+    statusIcon = <JoinedLeftIcon height={dimen} width={dimen} />;
+  }
+
+  let gm = new Map();
+  for (let m of activity.stats.extended.medals) {
+    if (m.info.isGold) {
+      gm.set(m.id, m.info);
+    }
+  }
+
+  let goldMedals = Array.from(gm.values());
+
+  if (goldMedals.length > 6) {
+    goldMedals = goldMedals.slice(0, 5);
+  }
+
+  let title = "Highest Value";
+  let killsHighlight = activity.stats.kills === summary.highestKills;
+  let killsTitle = killsHighlight ? title : "";
+
+  let assistsHighlight = activity.stats.assists === summary.highestAssists;
+  let assistsTitle = assistsHighlight ? title : "";
+
+  let defeatsHighlight =
+    activity.stats.opponenentsDefeated === summary.highestOpponentsDefeated;
+  let defeatsTitle = defeatsHighlight ? title : "";
+
+  let deathsHighlight = activity.stats.deaths === summary.highestDeaths;
+  let deathsTitle = deathsHighlight ? title : "";
+
+  let kdHighlight =
+    activity.stats.killsDeathsRatio.toFixed(2) ===
+    summary.highestKillsDeathsRatio.toFixed(2);
+  let kdTitle = kdHighlight ? title : "";
+
+  let effHighlight =
+    activity.stats.efficiency.toFixed(2) ===
+    summary.highestEfficiency.toFixed(2);
+  let effTitle = effHighlight ? title : "";
+
+  let [isExpanded, setIsExpanded] = useState(
+    props.expanded === undefined ? false : props.expanded
+  );
+
+  const onItemClick = function () {
+    console.log(isExpanded, !isExpanded);
+    setIsExpanded(!isExpanded);
+  };
+
+  let detailStyle = {
+    width: `${WIDTH - 40}px`,
+    height: "100px",
+    backgroundColor: "var(--list-item-background-color)",
+    border: "var(--list-item-border)",
+    borderTopWidth: "0px",
+    selfAlign: "center",
+  };
+
+  /*
+  let visibility = "visible";
+  let display = "display";
+  if (!isExpanded) {
+    visibility = "hidden";
+    display = "none";
+  }
+
+  detailStyle.visibility = visibility;
+  detailStyle.display = display;
+  */
+
+  let detailsDiv = isExpanded ? <div style={detailStyle}>foo</div> : "";
+
+  return (
+    <div style={gameContainerWrapper}>
+      <div style={gameContainerStyle} onClick={onItemClick}>
+        <div style={resultStyle}></div>
+        <div style={gameTitleStyle}>
+          <div className="list_title">{activity.activity.map.name}</div>
+          <div className="list_subtitle">{activity.activity.mode.label}</div>
+        </div>
+
+        <div style={statsStyle}>
+          <Stat
+            label="kills"
+            value={activity.stats.kills}
+            highlight={killsHighlight}
+            title={killsTitle}
+          />
+          <Stat
+            label="assists"
+            value={activity.stats.assists}
+            highlight={assistsHighlight}
+            title={assistsTitle}
+          />
+          <Stat
+            label="defeats"
+            value={activity.stats.opponentsDefeated}
+            highlight={defeatsHighlight}
+            title={defeatsTitle}
+          />
+          <Stat
+            label="deaths"
+            value={activity.stats.deaths}
+            highlight={deathsHighlight}
+            title={deathsTitle}
+          />
+          <Stat
+            label="kd"
+            value={activity.stats.killsDeathsRatio.toFixed(2)}
+            highlight={kdHighlight}
+            title={kdTitle}
+          />
+          <Stat
+            label="eff"
+            value={activity.stats.efficiency.toFixed(2)}
+            highlight={effHighlight}
+            title={effTitle}
+          />
+        </div>
+
+        <div style={statusStyle}>
+          <div>{statusIcon}</div>
+          <div>{mercyIcon}</div>
+        </div>
+        <div style={medalsStyle}>
+          {goldMedals.map((medal, index) => {
+            return (
+              <img
+                key={index}
+                src={medal.icon}
+                alt={medal.description}
+                title={`${medal.name.toUpperCase()} - ${medal.description}`}
+                height="14"
+              />
+            );
+          })}
+        </div>
+      </div>
+      {detailsDiv}
+    </div>
+  );
+};
+
+export default ActivityListItem;
