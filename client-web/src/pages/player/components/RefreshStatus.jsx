a3db5f0d2ed041ad21cb28485887ef889e6c6637
Mike Chambers <mikechambers@gmail.com>
2022-08-01 11:58:32 -0700

    Data loading status for page view

--- client-web/src/pages/player/components/RefreshStatus.jsx
+++ client-web/src/pages/player/components/RefreshStatus.jsx
@@ -1,34 +1,84 @@
 import { DateTime, Duration } from "luxon";
 import { useEffect, useState } from "react";
+import { calculatePercent } from "../../../utils";
 
 const RefreshStatus = (props) => {
   let lastUpdate = props.lastUpdate;
 
-  const [elapsedHuman, setElapsedHuman] = useState();
+  const [elapsedTime, setElapsedTime] = useState();
   useEffect(() => {
     if (!lastUpdate) {
       return;
     }
 
+    /*
     const interval = setInterval(() => {
-      //   /let ms = lastUpdate.getTime() - new Date().getTime();
-
-      let out = DateTime.fromJSDate(lastUpdate).toRelative();
-
-      //let out = d.toHuman({ listStyle: "long" });
-      setElapsedHuman(out);
-    }, 1000);
+      let t = Date.now() - lastUpdate.getTime();
+      setElapsedTime(t);
+    }, 100);
     return () => {
       clearInterval(interval);
+    };
+    */
+
+    let lastUpdateMs = lastUpdate.getTime();
+    const frameCallback = (elapsed) => {
+      let t = Date.now() - lastUpdateMs;
+
+      setElapsedTime(t);
+
+      let totalTime = 30 * 1000;
+      if (t >= totalTime) {
+        return;
+      }
+
+      window.requestAnimationFrame(frameCallback);
+    };
+
+    const intervalId = window.requestAnimationFrame(frameCallback);
+
+    //todo: issue is we only capture the first interval
+    return () => {
+      window.cancelAnimationFrame(intervalId);
     };
   }, [lastUpdate]);
 
   let s = "Waiting to update";
-  if (elapsedHuman) {
-    s = `Last updated ${elapsedHuman}`;
+  let percent = 0;
+  if (lastUpdate) {
+    let out = DateTime.fromJSDate(lastUpdate).toRelative();
+    s = `Last updated ${out}`;
+
+    let totalTime = 30 * 1000; // 30 seconds
+    percent = calculatePercent(elapsedTime, totalTime);
   }
 
-  return <div>{s}</div>;
+  let elementStyle = {
+    padding: "24px",
+  };
+  let barContainerStyle = {
+    width: "500px",
+    backgroundColor: "#FFFFFF22",
+  };
+
+  let barStyle = {
+    width: `${percent}%`,
+    backgroundColor: "#FFFFFF88",
+    height: "5px",
+  };
+
+  let timeElapsedStyle = {
+    font: "var(--font-progress)",
+  };
+
+  return (
+    <div style={elementStyle}>
+      <div style={barContainerStyle}>
+        <div style={barStyle}></div>
+      </div>
+      <div style={timeElapsedStyle}>{s}</div>
+    </div>
+  );
 };
 
 export default RefreshStatus;
