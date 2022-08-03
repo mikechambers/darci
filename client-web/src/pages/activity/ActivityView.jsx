e4bc5b45b5de0f72a2110b217ceec6a38653b193
Mike Chambers <mikechambers@gmail.com>
2022-08-03 09:28:20 -0700

    Initial structure for page

--- client-web/src/pages/activity/ActivityView.jsx
+++ client-web/src/pages/activity/ActivityView.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useFetchActivity } from "../../hooks/remote";
+import PageViewNavigation from "../player/components/PageViewNavigation";
 
 const ActivityView = (props) => {
   const params = useParams();
@@ -22,21 +23,64 @@ const ActivityView = (props) => {
     );
   }
 
+  const pageContainerStyle = {
+    minWidth: "720px",
+    padding: "0px var(--page-container-padding)",
+    background:
+      "linear-gradient(180deg, var(--background-color) 0%, rgba(54,54,54,1) 100%)",
+  };
+
+  const gappedStyle = {
+    display: "flex",
+    flexDirection: "column",
+    gap: "30px",
+  };
+
+  const pageLinks = [
+    {
+      value: "Overview",
+      id: "overview",
+    },
+    {
+      value: "Weapons",
+      id: "weapons",
+    },
+    {
+      value: "Meta Weapons",
+      id: "meta",
+    },
+    {
+      value: "medals",
+      id: "medals",
+    },
+    {
+      value: "maps",
+      id: "maps",
+    },
+    {
+      value: "games",
+      id: "games",
+    },
+  ];
+
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Activity</h2>
-      <div>Activity Id : {activityId}</div>
-      <div>
-        <a href={`https://destinytracker.com/destiny-2/pgcr/${activityId}`}>
-          Destiny Tracker
-        </a>
+    <div style={pageContainerStyle}>
+      <div style={gappedStyle}>
+        <PageViewNavigation links={pageLinks} />
+        <h2>Activity</h2>
+        <div>Activity Id : {activityId}</div>
+        <div>
+          <a href={`https://destinytracker.com/destiny-2/pgcr/${activityId}`}>
+            Destiny Tracker
+          </a>
+        </div>
+        <div>
+          <a href={`https://www.bungie.net/en/PGCR/${params.activityId}`}>
+            Bungie PGCR
+          </a>
+        </div>
       </div>
-      <div>
-        <a href={`https://www.bungie.net/en/PGCR/${params.activityId}`}>
-          Bungie PGCR
-        </a>
-      </div>
-    </main>
+    </div>
   );
 };
 
