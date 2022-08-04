da4ac1f7a25b5c77e647388220c02ceb9edab153
Mike Chambers <mikechambers@gmail.com>
2022-08-03 17:33:48 -0700

    working on activity layout

--- client-web/src/pages/activity/ActivityView.jsx
+++ client-web/src/pages/activity/ActivityView.jsx
@@ -81,10 +81,12 @@ const ActivityView = (props) => {
   };
 
   let scoreStyle = {
+    padding: "var(--content-padding)",
     height: "33%",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
+    gap: "6px",
   };
 
   let spacerContainerStyle = {
@@ -143,20 +145,68 @@ const ActivityView = (props) => {
     font: "var(--font-activity-period)",
   };
 
-  //todo: need to test this
+  //todo: need to test this and move to Activity
   let activityDuration = teams[0].players[0].stats.activityDurationSeconds;
   activityDuration = humanDuration(activityDuration * 1000);
+
+  let completionReason = teams[0].players[0].stats.completionReason;
 
   //let period = DateTime.fromJSDate(details.period).toRelative();
 
   let period = DateTime.fromJSDate(details.period).toFormat("ff");
 
+  let teamScoresStyle = {
+    display: "flex",
+    flexDirection: "rows",
+    gap: "10px",
+  };
+
+  let scoreBoxBaseStyle = {
+    width: 60,
+    height: 30,
+    border: "1px solid #ffffff88",
+    font: "var(--font-activity-score-box)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  let alphaScoreBoxStyle = {
+    ...scoreBoxBaseStyle,
+    backgroundColor: "#567CC6ee",
+  };
+
+  let betaScoreBoxStyle = {
+    ...scoreBoxBaseStyle,
+    backgroundColor: "#9D3627ee",
+  };
+
+  let scoreDivider = {
+    borderColor: "#ffffff",
+    borderStyle: "solid",
+    borderWidth: "0px 1px 0px 0px",
+  };
+
+  let completionReasonStyle = {
+    font: "var(--font-activity-completion-reason)",
+    textTransform: "uppercase",
+  };
+
+  console.log(activity);
   return (
     <div style={pageContainerStyle}>
       <div style={gappedStyle}>
         <PageViewNavigation links={pageLinks} />
         <div style={summaryStyle}>
-          <div style={scoreStyle}>score</div>
+          <div style={scoreStyle}>
+            <div style={completionReasonStyle}>{completionReason.label}</div>
+            <div style={teamScoresStyle}>
+              <div style={alphaScoreBoxStyle}>150</div>
+              <div style={scoreDivider}></div>
+              <div style={betaScoreBoxStyle}>92</div>
+            </div>
+            <div>{activityDuration}</div>
+          </div>
           <div style={spacerContainerStyle}></div>
           <div style={gameInfoContainterStyle}>
             <div style={gameInfoStyle}>
@@ -172,7 +222,6 @@ const ActivityView = (props) => {
               </div>
             </div>
             <div style={matchTimeStyle}>
-              <div>{activityDuration}</div>
               <div style={periodStyle}>{period}</div>
             </div>
           </div>
