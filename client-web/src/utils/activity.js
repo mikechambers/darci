4e6ee853cc3835329f4ba3d8d1218ae4ccb3ad1b
Mike Chambers <mikechambers@gmail.com>
2022-08-04 12:55:14 -0700

    remove unecessary code
    opponentDefeated is sent from the server, so we dont need to calculate

--- client-web/src/utils/activity.js
+++ client-web/src/utils/activity.js
@@ -1,30 +1,30 @@
 const { CompletionReason, Standing } = require("shared");
 
 const {
-    calculateEfficiency, calculateKillsDeathsRatio, calculateKillsDeathsAssists } = require("shared");
-
+  calculateEfficiency,
+  calculateKillsDeathsRatio,
+  calculateKillsDeathsAssists,
+} = require("shared");
 
 //note this modifies the passed in reference (to save memory / cpu)
 export const calculateStats = (stats, mode) => {
+  let standing = Standing.fromIdAndMode(stats.standing, mode);
+  stats.standing = standing;
 
-    let standing = Standing.fromIdAndMode(stats.standing, mode);
-    stats.standing = standing;
+  let completionReason = CompletionReason.fromId(stats.completionReason);
+  stats.completionReason = completionReason;
 
-    let completionReason = CompletionReason.fromId(stats.completionReason);
-    stats.completionReason = completionReason;
+  let kills = stats.kills;
+  let assists = stats.assists;
+  let deaths = stats.deaths;
 
-    let kills = stats.kills;
-    let assists = stats.assists;
-    let deaths = stats.deaths;
+  stats.efficiency = calculateEfficiency(kills, deaths, assists);
+  stats.killsDeathsRatio = calculateKillsDeathsRatio(kills, deaths);
+  stats.killsDeathsAssists = calculateKillsDeathsAssists(
+    kills,
+    deaths,
+    assists
+  );
 
-    stats.opponentsDefeated = kills + assists;
-
-    stats.efficiency = calculateEfficiency(kills, deaths, assists);
-    stats.killsDeathsRatio = calculateKillsDeathsRatio(kills, deaths);
-    stats.killsDeathsAssists = calculateKillsDeathsAssists(
-        kills, deaths, assists,
-    );
-
-    return stats;
-}
-
+  return stats;
+};
