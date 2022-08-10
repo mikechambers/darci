26582ecebd824a37d4fdc45891233ee8e732236f
Mike Chambers <mikechambers@gmail.com>
2022-08-09 20:40:03 -0700

    Removing unused component

--- client-web/src/components/CompleteGameIcon.jsx
+++
@@ -1,9 +0,0 @@
-import { ReactComponent as Icon } from "./images/complete_game_icon.svg";
-
-const CompleteGameIcon = (props) => {
-  let width = props.width;
-  let height = props.height;
-  return <Icon width={width} height={height} title="Played complete game" />;
-};
-
-export default CompleteGameIcon;
