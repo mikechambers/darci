e3ccf5594cc6d6b3277a177e75f4239891de43d0
Mike Chambers <mikechambers@gmail.com>
2022-08-16 21:16:37 -0700

    Working on search screen

--- client-web/src/screens/search/index.js
+++ client-web/src/screens/search/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import CharacterClassSelectionSelect from "../../components/CharacterClassSelectionSelect";
 import ModeSelect from "../../components/ModeSelect";
 import MomentSelect from "../../components/MomentSelect";
@@ -26,16 +27,41 @@ const pageLinks = [
 ];
 
 const SearchView = (props) => {
+  const [player, setPlayer] = useState();
+  const [characterClass, setCharacterClass] = useState();
+  const [mode, setMode] = useState();
+  const [moment, setMoment] = useState();
+
+  const onPlayerSelectChange = function (p) {
+    setPlayer(p);
+  };
+
+  const onClassSelectChange = function (c) {
+    setCharacterClass(c);
+  };
+
+  const onModeSelectChange = function (m) {
+    setMode(m);
+  };
+
+  const onMomentSelectChange = function (m) {
+    setMoment(m);
+  };
+
   return (
     <div id="page_nav" className="page_containter" style={pageContainerStyle}>
       <div style={gappedStyle}>
         <ScreenNavigationView links={pageLinks} />
 
         <div>
-          <PlayerSelect />
-          <CharacterClassSelectionSelect />
-          <ModeSelect />
-          <MomentSelect />
+          <PlayerSelect label="player" onChange={onPlayerSelectChange} />
+          <CharacterClassSelectionSelect
+            label="class"
+            onChange={onClassSelectChange}
+          />
+          <ModeSelect label="Mode" onChange={onModeSelectChange} />
+          <MomentSelect label="Moment" onChange={onMomentSelectChange} />
+          <button>Search</button>
         </div>
       </div>
     </div>
