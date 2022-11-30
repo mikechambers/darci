98cba037057ea2f4b9f400e5cf047456a9b92788
Mike Chambers <mikechambers@gmail.com>
2022-11-29 22:20:46 -0800

    Fix typo in CSS

--- client-web/src/screens/overlay/index.jsx
+++ client-web/src/screens/overlay/index.jsx
@@ -16,7 +16,7 @@ const rootStyleBase = {
     top: 0,
     left: 0,
     height: "100%",
-    width: "100$",
+    width: "100%",
     padding: 24,
     visibility: "visible",
     boxSizing: "border-box",
